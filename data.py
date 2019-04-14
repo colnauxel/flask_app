13eb3657bf8bc63e32afd13ef311b156b11a6da0
colnauxel <xuanloc120297@gmail.com>
2019-04-14 13:31:58 +0700

    first commit login

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'One',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat accusantium ipsa, nostrum consectetur quas dolorum delectus voluptatem',
+            'author':'loc'
+
+        },
+           {
+            'id':2,
+            'title':'Two',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat accusantium ipsa, nostrum consectetur quas dolorum delectus voluptatem',
+            'author':'xuan'
+
+        },
+           {
+            'id':3,
+            'title':'Three',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat accusantium ipsa, nostrum consectetur quas dolorum delectus voluptatem',
+            'author':'le'
+
+        },
+    ]
+    return articles
